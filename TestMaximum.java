d7187a757968907a5d6d3707571fa1461c98655c
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-28 15:50:55 +0530

    Added Welcome Message

---
+++ TestMaximum.java
@@ -0,0 +1,8 @@
+package com.bridgelabzd16;
+
+public class TestMaximum {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the program to find maximum using Generics");
+	}
+}
