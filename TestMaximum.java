78d17d227fd88d326c762507d7957fa499c59053
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-28 18:22:55 +0530

    Added method to find maximum of 3 Integers

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,8 +1,20 @@
 package com.bridgelabzd16;
 
 public class TestMaximum {
-	
+	/* Function maximum
+	 * This function is used to find the largest number */
+	public static void maximum(Integer[] inputArray)
+	{
+		int largest=0;
+		for(Integer element : inputArray) {
+			if((element.compareTo(largest))>0)
+					largest=element;
+		}
+		System.out.println("The Largest Number is: "+largest);
+	}
+	// Main Function
 	public static void main(String[] args) {
-		System.out.println("Welcome to the program to find maximum using Generics");
+		Integer[] intArray = {158,298,5500};
+		maximum(intArray);
 	}
 }
