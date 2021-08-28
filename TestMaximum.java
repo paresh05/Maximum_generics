5fb1057e884bf672211b7667c1a18ea49c4f3eb7
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-28 19:06:30 +0530

    Added method to find maximum of 3 Floats

--- TestMaximum.java
+++ TestMaximum.java
@@ -2,7 +2,7 @@ package com.bridgelabzd16;
 
 public class TestMaximum {
 	/* Function maximum
-	 * This function is used to find the largest number */
+	 * This function is used to find the largest number of integer data type */
 	public static void maximum(Integer[] inputArray)
 	{
 		int largest=0;
@@ -12,9 +12,21 @@ public class TestMaximum {
 		}
 		System.out.println("The Largest Number is: "+largest);
 	}
-	// Main Function
+	/* Function maximum
+	 * This function is used to find the largest number of double. data type */
+	public static void maximum(Float[] inputArray)
+	{
+		float largest=0;
+		for(Float element : inputArray) {
+			if((element.compareTo(largest))>0)
+					largest=element;
+		}
+		System.out.println("The Largest Number is: "+largest);
+	}
+	
+	// Main Function 
 	public static void main(String[] args) {
-		Integer[] intArray = {158,298,5500};
-		maximum(intArray);
+		Float[] floatArray = {158.5f,550.5f,550.51f};
+		maximum(floatArray);
 	}
 }
