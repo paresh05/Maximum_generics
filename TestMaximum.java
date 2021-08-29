0bc20163ca1c5abce207215e260e38dce5902394
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-29 12:04:16 +0530

    Refactored all 3 methods to one generic method

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,44 +1,25 @@
 package com.bridgelabzd16;
 
 public class TestMaximum {
-	/* Function maximum
-	 * This function is used to find the largest number of integer data type */
-	public static void maximum(Integer[] inputArray)
+	/* Method maximum
+	 * This Generic method is used to find the maximum */
+	public static <E extends Comparable<E>> void maximum(E[] inputArray)
 	{
-		int largest=0;
-		for(Integer element : inputArray) {
-			if((element.compareTo(largest))>0)
-					largest=element;
+		E maximum = inputArray[0];
+		for(E element : inputArray) {
+			if((element.compareTo(maximum))>0)
+					maximum=element;
 		}
-		System.out.println("The Largest Number is: "+largest);
+		System.out.println("The Maximum is: "+maximum);
 	}
-	/* Function maximum
-	 * This function is used to find the largest number of float data type */
-	public static void maximum(Float[] inputArray)
-	{
-		float largest=0;
-		for(Float element : inputArray) {
-			if((element.compareTo(largest))>0)
-					largest=element;
-		}
-		System.out.println("The Largest Number is: "+largest);
-	}
-	/* Function maximum
-	 * This function is used to find the maximum Strings */
-	public static void maximum(String[] inputArray)
-	{
-		String largest=" ";
-		for(String element : inputArray) {
-			if((element.compareTo(largest))>0)
-					largest=element;
-		}
-		System.out.println("The Largest Number is: "+largest);
-	}
-	
 	
 	// Main Function 
 	public static void main(String[] args) {
 		String[] stringArray = {"Apple","Peach","Banana"};
+		Integer[] intArray = {778,52,45};
+		Float[] floatArray= {52.5f,53.2f,56.4f};
 		maximum(stringArray);
+		maximum(intArray);
+		maximum(floatArray);
 	}
 }
