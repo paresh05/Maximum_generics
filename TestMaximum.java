778b6b3f4d8cc6212df15001bdd8fe8efc6dbed3
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-31 16:18:40 +0530

    Added: PrintMax method to print maximum

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,33 +1,45 @@
 package com.bridgelabzd16;
 
 public class TestMaximum<E extends Comparable<E>> {
-	
+
 	E[] inputArray;
+
+	// Parameterized constructor
 	public TestMaximum(E[] inputArray) {
-		this.inputArray=inputArray;
+		this.inputArray = inputArray;
 	}
-	
+
+	// The function maximum returns the maximum of the inputArray
 	public E maximum() {
 		return TestMaximum.maximum(inputArray);
 	}
-	/* Method maximum
-	 * This Generic method is used to find the maximum */
-	public static <E extends Comparable<E>>E maximum(E[] inputArray)
-	{
+
+	/*
+	 * Method maximum 
+	 * This Generic method is used to find the maximum
+	 */
+	public static <E extends Comparable<E>> E maximum(E[] inputArray) {
 		E maximum = inputArray[0];
-		for(E element : inputArray) {
-			if((element.compareTo(maximum))>0)
-					maximum=element;
+		for (E element : inputArray) {
+			if ((element.compareTo(maximum)) > 0)
+				maximum = element;
 		}
-		System.out.println("The Maximum is: "+maximum);
+		printMax(maximum);
 		return maximum;
 	}
-	
-	// Main Function 
+
+	// This function prints the maximum
+	public static <E> void printMax(E maximum) {
+		System.out.println("The Maximum is: " + maximum);
+	}
+
+	// Main Function
+
 	public static void main(String[] args) {
-		String[] stringArray = {"Apple","Peach","Banana","Orange"};
-		Integer[] intArray = {778,52,45,587,1008,998,1126};
-		Float[] floatArray= {52.5f,53.2f,56.4f};
+
+		String[] stringArray = { "Apple", "Peach", "Banana", "Orange" };
+		Integer[] intArray = { 778, 52, 45, 587, 1008, 998, 1126 };
+		Float[] floatArray = { 52.5f, 53.2f, 56.4f };
 		TestMaximum obj = new TestMaximum(stringArray);
 		obj.maximum();
 		TestMaximum obj2 = new TestMaximum(intArray);
