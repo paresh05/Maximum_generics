a2aea9d1e8efd8200e2e8fe92fe99ecff13597c0
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-31 15:56:37 +0530

    Refactor: Create Generic Class

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,9 +1,18 @@
 package com.bridgelabzd16;
 
-public class TestMaximum {
+public class TestMaximum<E extends Comparable<E>> {
+	
+	E[] inputArray;
+	public TestMaximum(E[] inputArray) {
+		this.inputArray=inputArray;
+	}
+	
+	public E maximum() {
+		return TestMaximum.maximum(inputArray);
+	}
 	/* Method maximum
 	 * This Generic method is used to find the maximum */
-	public static <E extends Comparable<E>> void maximum(E[] inputArray)
+	public static <E extends Comparable<E>>E maximum(E[] inputArray)
 	{
 		E maximum = inputArray[0];
 		for(E element : inputArray) {
@@ -11,6 +20,7 @@ public class TestMaximum {
 					maximum=element;
 		}
 		System.out.println("The Maximum is: "+maximum);
+		return maximum;
 	}
 	
 	// Main Function 
@@ -18,8 +28,11 @@ public class TestMaximum {
 		String[] stringArray = {"Apple","Peach","Banana"};
 		Integer[] intArray = {778,52,45};
 		Float[] floatArray= {52.5f,53.2f,56.4f};
-		maximum(stringArray);
-		maximum(intArray);
-		maximum(floatArray);
+		TestMaximum obj = new TestMaximum(stringArray);
+		obj.maximum();
+		TestMaximum obj2 = new TestMaximum(intArray);
+		obj2.maximum();
+		TestMaximum obj3 = new TestMaximum(floatArray);
+		obj3.maximum();
 	}
 }
